e13e85616616094182dd565b61c6f976f69b5e5c
José Ribeiro <joseribeirocsr@gmail.com>
2025-06-28 13:56:52 -0300

    Versão final do pokedex

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,27 +1,74 @@
 const pokeApi = {};
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-  const pokemon = new Pokemon();
-  pokemon.number = pokeDetail.id;
-  pokemon.name = pokeDetail.name;
+const getPokemonImage = (data) => {
+  return (
+    data.sprites?.other?.dream_world?.front_default ||
+    data.sprites?.other?.['official-artwork']?.front_default ||
+    data.sprites?.front_default ||
+    ''
+  );
+};
 
+async function convertPokeApiDetailToPokemon(pokeDetail) {
   const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
   const [type] = types;
-  pokemon.types = types;
-  pokemon.type = type;
 
-  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+  const pokemon = {
+    number: pokeDetail.id,
+    name: pokeDetail.name,
+    types: types,
+    type: type,
+    photo: getPokemonImage(pokeDetail),
+    abilities: pokeDetail.abilities || [],
+    stats: pokeDetail.stats || [],
+    moves: pokeDetail.moves || [],
+    height: pokeDetail.height,
+    weight: pokeDetail.weight,
+    evolutions: [], // Preenchido abaixo
+  };
 
-  // Adicionando mais dados para o modal:
-  pokemon.abilities = pokeDetail.abilities;
-  pokemon.stats = pokeDetail.stats;
-  pokemon.moves = pokeDetail.moves;
-  pokemon.height = pokeDetail.height;
-  pokemon.weight = pokeDetail.weight;
+  function parseEvolutions(chain) {
+    const evolutions = [];
+
+    function traverse(node) {
+      if (!node) return;
+
+      evolutions.push(node.species.name);
+
+      node.evolves_to.forEach((child) => traverse(child));
+    }
+
+    traverse(chain);
+    return evolutions;
+  }
+
+  try {
+    const speciesResponse = await fetch(pokeDetail.species.url);
+    const speciesData = await speciesResponse.json();
+
+    const evolutionResponse = await fetch(speciesData.evolution_chain.url);
+    const evolutionData = await evolutionResponse.json();
+
+    const evolutionNames = parseEvolutions(evolutionData.chain);
+
+    const evolutionDetails = await Promise.all(
+      evolutionNames.map(async (name) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const data = await res.json();
+        return {
+          name: data.name,
+          photo: getPokemonImage(data),
+        };
+      })
+    );
+
+    pokemon.evolutions = evolutionDetails;
+  } catch (error) {
+    console.error("Erro ao buscar evoluções:", error);
+  }
 
   return pokemon;
 }
-
 
 pokeApi.getPokemonDetail = (pokemon) => {
   return fetch(pokemon.url)
@@ -38,4 +85,19 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails);
+};
+
+const buscaPoke = (name) => {
+  const namePoke = valueInput || name;
+  const url = `https://pokeapi.co/api/v2/pokemon/${namePoke}`;
+
+  pokeApi
+    .getPokemonDetail({ url })
+    .then((pokemonData) => {
+      setPoke(pokemonData);
+      setTodosPoke(null);
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar Pokémon:", error);
+    });
 };
